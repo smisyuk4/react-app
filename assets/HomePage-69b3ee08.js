52151050bf49f3b119d618836bf7fc048f23ea3f
smisyuk4 <smisyuk4@users.noreply.github.com>
2023-06-18 21:06:24 +0000

    deploy: 364d7d0fa19f99289d1d41ac9aa7c6adb8ce9d0f

---
+++ assets/HomePage-69b3ee08.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-6c15e88a.js";const o=()=>e.jsx("div",{children:e.jsx("h1",{children:" Home page"})});export{o as HomePage,o as default};
---
+++ assets/WelcomePage-242b5caa.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-6c15e88a.js";const o=()=>e.jsx("div",{children:e.jsx("h1",{children:" Welcome page"})});export{o as default};

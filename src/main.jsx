d270f1c86500655116cd4db755ec6273b0b2ad1a
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-07-16 22:47:59 +0300

    change script gh action for copy index.html to 404.html

--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,10 @@ import { store } from './redux/store';
 import { App } from './components/App';
 import './index.css';
 
-const baseUrl =
-  window.location.host === 'http://127.0.0.1:5173/react-app/'
-    ? '/react-app'
-    : '/react-app';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename={baseUrl}>
+      <BrowserRouter basename="/react-app">
         <App />
       </BrowserRouter>
     </Provider>
